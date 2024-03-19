f411fe50c97be730fa34c275560eb1a7e06f0731
Abdelrhman Amro <abdamro288dev@gmail.com>
2024-03-19 22:44:58 +0200

    ok check now

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,17 @@
 # Install an especific version of flask (2.1.0)
 
-package {'flask':
+package { 'python3-pip':
+  ensure => installed,
+}
+
+package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+  require  => Package['python3-pip'],
+}
+
+package { 'werkzeug':
+  ensure   => '2.0.3', # Adjust version as needed
+  provider => 'pip3',
+  require  => Package['python3-pip'],
 }
